149f4c2541e33eebfa7b4b32337da7ef9e5a46b1
Bill Wang <ozbillwang@gmail.com>
2015-11-25 18:32:31 +1100

    bugfix/#9 - bug about the USER directive
    bugfix/#9 - bug about the USER directive

--- dockerfile-from-image.rb
+++ dockerfile-from-image.rb
@@ -54,7 +54,7 @@ loop do
     cmd = cmd.last
 
     if cmd.start_with?(NOP_PREFIX)
-      commands << cmd.split(NOP_PREFIX).last
+      commands << cmd.gsub(NOP_PREFIX,'').gsub(/^USER[ |\t]+\[(.*)\]/,'USER \1')
     else
       commands << "RUN #{cmd}".squeeze(' ')
     end
